c7011f786c2665aaa4e166d93b9b049dd58c43b6
lruiabc <568047669@qq.om>
2015-10-04 16:58:06 -0500

    Added code to pull new index.html and page2.html from other firl

--- install-app.sh
+++ install-app.sh
@@ -5,7 +5,8 @@ sudo apt-get install -y apache2 git
 
 git clone https://github.com/lruiabc/itmo-544-444-fall2015.git
 
-mv ./itmo-544-444-fall2015/page2 /var/www/html
+mv ./itmo-544-444-fall2015/page2.html /var/www/html
+mv ./itmo-544-444-fall2015/index.html /var/www/html
 
 echo "Hello2" > /tmp/Hello2.txt
 
